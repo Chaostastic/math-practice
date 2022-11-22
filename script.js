954426fe583ed922f1d0c2d3526db359861cf48d
Chaostastic <roodey72mc@gmail.com>
2022-11-22 09:22:14 +0200

    Check if answer is correct

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const answerInput = document.querySelector("#answer")
+const answerSubmit = document.querySelector("#answer-submit")
+
 const wildCards = {
     x1: randomInt(-9, 9),
     y1: randomInt(-9, 9),
@@ -7,14 +10,25 @@ const wildCards = {
     z2: randomInt(-9, 9),
 }
 
+const answer = round(Math.sqrt(Math.pow((wildCards.x1-wildCards.x2),2)+Math.pow((wildCards.y1-wildCards.y2),2)+Math.pow((wildCards.z1-wildCards.z2),2)), 1)
+
+function round(num, decimal) {
+    return Math.round(num * Math.pow(10, decimal)) /  Math.pow(10, decimal)
+}
+
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function replaceWild(element) {
-    for (const key in wildCards) {
-        element.textContent = element.textContent.replace(`{${key}}`, wildCards[key])
+function replaceWild(element, wild) {
+    for (const key in wild) {
+        element.textContent = element.textContent.replace(`{${key}}`, wild[key])
     }
 }
 
-replaceWild(document.querySelector("#exercise"))
+function checkAnswer() {
+    answerInput.classList.add((+answerInput.value === answer) ? "correct" : "false")
+}
+
+replaceWild(document.querySelector("#exercise"), wildCards)
+answerSubmit.addEventListener("click", checkAnswer)
